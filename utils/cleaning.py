5fc84f9d4c79703874fd54d4b28f267105197544
Paul Balluff <paul.balluff@univie.ac.at>
2023-06-29 16:05:53 +0200

    fix regex

--- utils/cleaning.py
+++ utils/cleaning.py
@@ -3,7 +3,7 @@ import re
 LINK_REGEX = re.compile('\b(?:https?:\/\/|www\.)(?:[\w-]+\.)*[\w-]+(?:\.(?:at|com|de|co))\b')
 EMAIL_REGEX = re.compile(r'\S+@\S+')
 QUOTATION_MARKS = re.compile(r"([“”‘’«»„“”‹›❝❞〝〞〟＂＇«»‹›〈〉《》「」『』【】〔〕〖〗〘〙〚〛‘’‚‘’‛“”„“”‟‘’“”‘’„“”‹›«»«»“”»«»“”»«»“”‹›‘’‘’‘’‚‘’‚‘’‛‘’‛‘’“”„“”„“”‟„“”‟‘’‹›‹›])")
-PUNCTUATION = re.compile(r'([\!\"#$%&\'()*+,-–\.…/:;<=>\?@\[\\\]^_`\{\|\}\~])')
+PUNCTUATION = re.compile(r'([!"#$%&\'()*+,-.…/:;<=>?@\[\\\]^_`{|–}~])')
 NUMBERS = re.compile(r'\d+')
 WHITESPACE = re.compile(r'\s{2,}')
 LINE_BREAKS = re.compile(r'\n')
@@ -12,5 +12,5 @@ LINE_BREAKS = re.compile(r'\n')
 if __name__ == "__main__":
     print('testing regex')
 
-    t1 = "Amazon prüft „weitere Konsequenzen“."
-    assert PUNCTUATION.sub(" ", t1) == "Amazon prüft  weitere Konsequenzen  ", PUNCTUATION.sub(" ", t1)
+    t1 = "Amazon prüft „weitere Konsequenzen“.…"
+    assert PUNCTUATION.sub(" ", t1) == "Amazon prüft „weitere Konsequenzen“  ", PUNCTUATION.sub(" ", t1)
