4881ed607ff189550d448718f428a1d7f4f9551c
Paul Balluff <paul.balluff@univie.ac.at>
2023-08-01 19:26:01 +0200

    get models from sql database

--- 04_eval/eval_cosine.py
+++ 04_eval/eval_cosine.py
@@ -38,17 +38,21 @@
 
 import typing
 
+from pathlib import Path
+import sqlalchemy
+
 import fasttext
 import random
 import numpy as np
 from scipy.spatial import distance
 from scipy.stats import pearsonr
 
-# TODO: 
-#    - load models via sql db
-#    - automatically compare all models with each other
-#    - store output in sql db
+from utils.sql import start_sqlsession
+from utils.datamodel import Model, WithinCorrelationResults, AcrossCorrelationResults
 
+p = Path.cwd()
+
+session, engine = start_sqlsession()
 
 def cosine_distance(model: fasttext.FastText._FastText, 
                     cues: typing.List[str],
@@ -101,20 +105,74 @@ def calc_correlation(model1: fasttext.FastText._FastText,
     return np.array(correlation)
 
 
+if __name__ == '__main__':
 
-model1 = fasttext.load_model("models/beliebte_annamaria_lr0.05_epochs3_mincount50_ws12_dims300.bin")
-model2 = fasttext.load_model("models/reicher_alper.bin")
+    # TODO: 
+    #    - load models via sql db
+    #    - automatically compare all models with each other
+    #    - store output in sql db
 
-assert len(model1.words) == len(model2.words)
 
-words_politics = ["Demokratie", "Freiheit", "Gleichheit", "Gerechtigkeit", "Einwanderung", "Schwangerschaftsabbruch", "Sozialstaat", "Steuern", "ÖVP", "SPÖ", "FPÖ", "Grüne", "NEOS"]
-words_random = random.sample(model1.words, 100)
+    # Within Correlations
 
-corr_politics = calc_correlation(model1, model2, words_politics)
-corr_random = calc_correlation(model1, model2, words_random)
+    # Get models from same family
 
-# 2. compare different initializations
-avg_sim_vectors1 = average_cosine_distance([model1, model2], words_politics)
-# avg_sim_vectors2 = average_cosine_distance([model3, model4], words_politics)
+    
 
-# correlation = [pearsonr(avg_sim_vectors1[i], avg_sim_vectors2[i])[0] for i in range(len(words_politics))]
+    sql_query_string = """
+    select A.id as model_a, B.id as model_b
+    from models A, models B
+    where A.id != B.id AND
+            A.training_corpus = B.training_corpus AND
+            A.model_type = B.model_type and
+            A.learning_rate = B.learning_rate and 
+            A.epochs = B.epochs AND
+            A.word_ngrams = B.word_ngrams AND 
+            A.min_count = B.min_count AND
+            A.window_size = B.window_size AND 
+            A.dimensions = B.dimensions
+    """
+
+    with engine.connect() as con:
+        model_combinations = con.execute(sqlalchemy.text(sql_query_string)).all()
+
+    print('Model combinations:', len(model_combinations))
+
+    model_a_meta = session.query(Model).where(Model.id == model_combinations[0][0]).first()
+    model_b_meta = session.query(Model).where(Model.id == model_combinations[0][1]).first()
+
+    # build a string that identifies the model parameters
+    # maybe put that into a separate function
+    corpus = Path(model_a_meta.training_corpus).name.replace('.txt', '')
+    parameter_string = f"{corpus}_epochs{model_a_meta.epochs}_lr{model_a_meta.learning_rate}_mincount{model_a_meta.min_count}_ws{model_a_meta.window_size}_dims{model_a_meta.dimensions}"
+
+    model_a = fasttext.load_model(model_b_meta.model_path + '.bin')
+    model_b = fasttext.load_model(model_b_meta.model_path + '.bin')
+
+    # ensure that we only have words that both models share
+    # get intersection of both vocabulary
+    shared_vocabulary = set(model_a.words) & set(model_b.words)
+
+    words_politics = ["Demokratie", "Gleichheit", "Gerechtigkeit",
+                    "Einwanderung", "Pension", "Sozialstaat", "Bildung",
+                    "Steuern", "ÖVP", "SPÖ", "FPÖ", "Grüne", "NEOS"]
+    
+    words_politics = set(words_politics) & shared_vocabulary
+
+    words_random = random.sample(shared_vocabulary, 100)
+
+    corr_politics = calc_correlation(model_a, model_b, words_politics)
+    corr_random = calc_correlation(model_a, model_b, words_random)
+
+    results = WithinCorrelationResults(model_a_id=model_a_meta.id, model_b_id=model_b_meta.id,
+                                       cues='politics',
+                                       correlation=corr_politics.mean(),
+                                       correlation_type="Pearson's Rho")
+    session.add(results)
+    session.commit()
+
+    # 2. compare different initializations
+    avg_sim_vectors1 = average_cosine_distance([model_a, model_b], words_politics)
+    # avg_sim_vectors2 = average_cosine_distance([model3, model4], words_politics)
+
+    # correlation = [pearsonr(avg_sim_vectors1[i], avg_sim_vectors2[i])[0] for i in range(len(words_politics))]
