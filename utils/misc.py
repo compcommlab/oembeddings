b11949193578b1548f2149e67707f962abb6e7d6
Paul Balluff <paul.balluff@univie.ac.at>
2023-08-12 11:35:24 +0200

    add function for harmonic mean

--- utils/misc.py
+++ utils/misc.py
@@ -4,3 +4,6 @@ import hashlib
 def md5sum(text: str) -> str:
     text_md5 = hashlib.md5(text.encode('utf-8'))
     return text_md5.hexdigest()
+
+def harmonic_mean(x: float, y: float) -> float:
+    return ((2 * x * y) / (x + y))
