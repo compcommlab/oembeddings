a445da2873872bf09f9663f0b0c1034d96c1811d
Paul Balluff <paul.balluff@univie.ac.at>
2023-08-21 10:15:53 +0200

    add more cues

--- evaluation_data/cues.py
+++ evaluation_data/cues.py
@@ -2,7 +2,12 @@
 # Used for evaluation of cosine similarity
 
 CUES = {
-    "politics" : ["Demokratie", "Gleichheit", "Gerechtigkeit",
-                "Einwanderung", "Pension", "Sozialstaat", "Bildung",
-                "Steuern", "ÖVP", "SPÖ", "FPÖ", "Grüne", "NEOS"]
+    "politics": ["Demokratie", "Gleichheit", "Gerechtigkeit",
+                 "Einwanderung", "Pension", "Sozialstaat", "Bildung",
+                 "Steuern", "ÖVP", "SPÖ", "FPÖ", "Grüne", "NEOS"],
+    "economy": ["Wirtschaft", "Steuer", "Teuerung", "Euro", "Banken", "Budget"],
+    "culture": ["Kunst", "Kultur", "Theater", "Zensur", "Festspiel", "Oper", "Konzert"],
+    "sports": ["Sport", "Fußball", "Skifahren", "Medaille", "Podest", "Sieg", "Niederlage"],
+    "environmnent": ["Klima", "Umwelt", "Schutz", "Nachhaltigkeit", "Energie", "Nationalpark"],
+    "migration": ["Einwanderung", "Asyl", "Migration", "Flucht", "Schlepperei", "Fremdenhass"],
 }
