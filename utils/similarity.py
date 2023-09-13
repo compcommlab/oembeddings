2d802f35bee6298312de752a962aae65b5a61c03
Paul Balluff <paul.balluff@univie.ac.at>
2023-09-13 09:09:24 +0200

    improve documentation and error handling

--- utils/similarity.py
+++ utils/similarity.py
@@ -5,7 +5,7 @@ from scipy.spatial import distance
 import fasttext
 
 def cosine_distance(model: fasttext.FastText._FastText,
-                    cues: typing.List[str],
+                    cues: typing.Iterable[str],
                     vocabulary: typing.Iterable = None,
                     normalize=True) -> np.ndarray:
     """ 
@@ -13,6 +13,9 @@ def cosine_distance(model: fasttext.FastText._FastText,
         cosine distance for each cue word against every word in the 
         model's vocabulary list.
     """
+    
+    assert len(cues) > 0, 'List of cues is empty!'
+
     v = np.array([model.get_word_vector(cue) for cue in cues])
     if vocabulary:
         v_vocab = np.array([model.get_word_vector(word)
@@ -27,7 +30,7 @@ def cosine_distance(model: fasttext.FastText._FastText,
 
 
 def average_cosine_distance(models: typing.List[fasttext.FastText._FastText],
-                            cues: typing.List[str],
+                            cues: typing.Iterable[str],
                             vocabulary: typing.Iterable = None) -> np.ndarray:
     """ 
         Given a list of models, this returns the mean cosine distance for
@@ -38,13 +41,15 @@ def average_cosine_distance(models: typing.List[fasttext.FastText._FastText],
 
     """
 
+    assert len(cues) > 0, 'List of cues is empty!'
+
     cos_sim = [cosine_distance(model, cues, vocabulary=vocabulary) for model in models]
     return np.mean(cos_sim, axis=0)
 
 
 def calc_correlation(model1: fasttext.FastText._FastText,
                      model2: fasttext.FastText._FastText,
-                     cues: typing.List[str],
+                     cues: typing.Iterable[str],
                      vocabulary: typing.Iterable = None) -> np.ndarray:
     """ 
 
